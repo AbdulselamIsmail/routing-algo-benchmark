71cca706cfe3b9f761d9155d0af9ab8d55344433
Abdülselam_İsmail <147073125+AbdulselamIsmail@users.noreply.github.com>
2025-12-10 23:39:34 +0300

    Added constructor

--- SimulationResult.java
+++ SimulationResult.java
@@ -6,4 +6,13 @@ public class SimulationResult {
     public long durationNano;    // Execution time
     public int nodesVisited;     // Efficiency metric
     public int pathLength;       // To verify correctness
+
+    // Add this Constructor so you can create the object in one line
+    public SimulationResult(String name, int size, long time, int visited, int length) {
+        this.algorithmName = name;
+        this.graphSize = size;
+        this.durationNano = time;
+        this.nodesVisited = visited;
+        this.pathLength = length;
+    }
 }
